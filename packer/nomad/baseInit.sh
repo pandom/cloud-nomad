2a234a3eac71c8ba1ea46c37d53f1f4be3865ee8
Anthony Burke <burkey@hashicorp.com>
2020-04-13 11:34:25 +1000

    packer nodes for Nomad

--- packer/nomad/baseInit.sh
+++ packer/nomad/baseInit.sh
@@ -4,7 +4,10 @@ main() {
   sudo apt-get update
   sudo apt-get install -y jq
   sudo apt-get install -y unzip
+  #unzip nomad
   sudo unzip /tmp/nomad-enterprise_0.11.0+ent_linux_amd64.zip
+  #move nomad
+  
 }
 
 main
