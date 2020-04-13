8d44fd87f716e13742043ed123b32cc7ecd9b06b
Anthony Burke <burkey@hashicorp.com>
2020-04-13 11:46:37 +1000

    real variables :|

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,59 @@
-variable "prefix" {
-  description = "This prefix will be included in the name of most resources."
+variable "consul" {
+  default = {
+    version              = "1.7.2"
+    servers_count        = 3
+    server_instance_type = "t3.micro"
+  }
+  type = object({
+    version              = string
+    servers_count        = number
+    server_instance_type = string
+  })
+}
+
+variable "nomad" {
+  default = {
+    version              = "0.11.0"
+    servers_count        = 3
+    server_instance_type = "t3.micro"
+    clients_count        = 5
+    client_instance_type = "t3.micro"
+  }
+  type = object({
+    version              = string
+    servers_count        = number
+    server_instance_type = string
+    clients_count        = number
+    client_instance_type = string
+  })
 }
 
 variable "region" {
-  description = "The region where the resources are created."
-  default     = "ap-southeast-2"
+  default = "ap-southeast-2"
+  type    = string
 }
 
-variable "address_space" {
-  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = "10.0.0.0/16"
+variable "namespace" {
+  default = "burkey"
+  type    = string
 }
 
-variable "subnet_prefix" {
-  description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
+variable "ssh_keypair" {
+  default = null
+  type    = string
+}
+
+variable "datacenter" {
+  default = "aws"
+  type    = string
+}
+
+variable "join_wan" {
+  type    = list(string)
+  default = []
+}
+
+variable "associate_public_ips" {
+  default = true
+  type = bool
 }
