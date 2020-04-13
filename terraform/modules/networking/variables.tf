f700da4279c1cbf2f8b975c2472872b0e2fff969
Anthony Burke <burkey@hashicorp.com>
2020-04-13 14:51:13 +1000

    Converted to a CIDR

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -1,8 +1,8 @@
 variable "namespace" {
   type = string
 }
-
+#CIDR value for ingress of a single IP
 variable "myip" {
   type = string
-  default = "120.148.1.90"
+  default = "120.148.1.90/32"
 }
